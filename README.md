e0ec09eb6e0ffbbfcf1f18b1ad73d83a39c4b832
Cyd <34154246+cyd01@users.noreply.github.com>
2020-01-13 00:32:13 +0100

    import initial

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# markdown-editor
+[//]: # (title:markdown-editor)
+
+# Welcome to markdown-editor
+
+This markdown-editor is a 99% client-side javascript viewer and editor for markdown.
+
+Why 1% missing. Because when you finished to edit your document you can upload it directly to your web server.
+
+## How to compile ?
+
+Just clone the repository and run the **compile.sh** provided script file.
+
+```
+git clone git@github.com:cyd01/markdown-editor.git
+cd markdown-editor
+./compile.sh
+```
+
+It produces a new file **install.php**. Just upload it on your web site and call it.
+
+## How does it work ?
+
+- To switch from page viewer to page editor, just double-click
+- To protect the page with password ALT+p
+- To save the page press ALT+o
+- To switch back to the viewer press escape key
+
---
+++ compile.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+{
+
+sed  '/INCLUDE_START/,$d' edit.html
+
+echo -n '<link rel="icon" type="image/x-icon" href="data:image/x-icon;base64,' ; base64 favicon.ico|tr -d '\n' ; echo '">'
+echo -n '<style type="text/css">' ; cat css/style.min.css | tr -d '\n' ; echo '</style>'
+
+echo -n '<script type="text/javascript">' ; cat js/base64.min.js | tr -d '\n' ; echo '</script>'
+echo -n '<script type="text/javascript">' ; cat js/vue.min.js | tr -d '\n' ; echo '</script>'
+echo -n '<script type="text/javascript">' ; cat js/marked.min.js | tr -d '\n' ; echo '</script>'
+echo -n '<script type="text/javascript">' ; cat js/sweetalert2.all.min.js | tr -d '\n' ; echo '</script>'
+echo -n '<script type="text/javascript">' ; cat js/lodash.min.js | tr -d '\n' ; echo '</script>'
+echo -n '<script type="text/javascript">' ; cat js/com.js ; echo '</script>'
+echo -n '<script type="text/javascript">' ; cat js/sjcl.js ; echo '</script>'
+
+sed  '1,/INCLUDE_END/d' edit.html
+
+} > index.html
+
+{
+echo '<?php'
+
+echo '/* Markdown Editor Installer */' ;
+echo -n '$index="' ; base64 index.html|tr -d '\n' ; echo '";' ;
+echo -n '$htaccess="' ; base64 .htaccess|tr -d '\n' ; echo '";' ;
+echo -n '$edit="' ; base64 edit.php|tr -d '\n' ; echo '";' ;
+echo -n '$fav="' ; base64 favicon.ico|tr -d '\n' ; echo '";' ;
+echo
+echo 'if( !file_exists(".htaccess") ) { file_put_contents( ".htaccess", base64_decode($htaccess) ) ; }'
+echo 'if( !file_exists("favicon.ico") ) { file_put_contents( "favicon.ico", base64_decode($fav) ) ; }'
+echo 'if( !file_exists("index.html") ) { file_put_contents( "index.html", base64_decode($index) ) ; }'
+echo 'if( !file_exists("pass") ) { mkdir("pass") ; }'
+echo 'if( !file_exists("pass/.htaccess") ) { file_put_contents( "pass/.htaccess", "deny from all" ) ; }'
+echo 'header("Location: index.html");'
+
+echo '?>'
+} > install.php
