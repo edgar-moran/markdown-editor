2c3d8ae13f44028f3e7abd427bb196b353ad6379
Cyd <34154246+cyd01@users.noreply.github.com>
2020-01-26 19:50:02 +0100

    Maj

--- js/com.js
+++ js/com.js
@@ -98,3 +98,38 @@ function savePage(url, page, data, fctok,fctko) {
 	var Params = "page="+page+"&data="+encodeURIComponent(data) ;
 	xhr.send( Params ) ;
 }
+function loadjscssfile(filename, filetype, title) {
+    if (filetype=="js"){ //if filename is a external JavaScript file
+        var fileref=document.createElement('script')
+        fileref.setAttribute("type","text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype=="css"){ //if filename is an external CSS file
+        var fileref=document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+	fileref.setAttribute("title", title)
+        fileref.setAttribute("href", filename)
+    }
+    if (typeof fileref!="undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
+/*
+loadjscssfile("myscript.js", "js") //dynamically load and add this .js file
+loadjscssfile("javascript.php", "js") //dynamically load "javascript.php" as a JavaScript file
+loadjscssfile("mystyle.css", "css") ////dynamically load and add this .css file
+*/
+function removejscssfile(filename, filetype){
+    var targetelement=(filetype=="js")? "script" : (filetype=="css")? "link" : "none" //determine element type to create nodelist from
+    var targetattr=(filetype=="js")? "src" : (filetype=="css")? "href" : "none" //determine corresponding attribute to test for
+    var allsuspects=document.getElementsByTagName(targetelement)
+    for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+    if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf(filename)!=-1)
+        allsuspects[i].parentNode.removeChild(allsuspects[i]) //remove element by calling parentNode.removeChild()
+    }
+}
+/*
+removejscssfile("somescript.js", "js") //remove all occurences of "somescript.js" on page though in most browser this does not unload the script
+removejscssfile("somestyle.css", "css") //remove all occurences "somestyle.css" on page
+*/
+
